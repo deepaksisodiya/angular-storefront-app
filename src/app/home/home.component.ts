e8ef5d50fece014cf1b7f47e265a0da34a888df4
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2024-07-26 16:57:55 +0530

    learning angular

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { Product, Products } from '../../types';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  constructor(private productsService: ProductsService) {}
 
+  rows: number = 12;
+  products: Product[] = [];
+  totalRecords: number = 0;
+
+  ngOnInit() {
+    this.fetchProducts(0, this.rows);
+  }
+
+  fetchProducts(page: number, perPage: number) {
+    this.productsService
+      .getProducts('http://localhost:3000/clothes', { page, perPage })
+      .subscribe({
+        next: (data: Products) => {
+          this.products = data.items;
+          this.totalRecords = data.total;
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
+  }
 }
